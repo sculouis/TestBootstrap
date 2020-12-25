d8c9887f468c22444519120e36cd30e9be9fae38
sculouis <scu.louis@gmail.com>
2020-12-25 16:23:08 +0800

    basic js program

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-* bootstrap 4 的教學程式碼
+# bootstrap 4 的教學程式碼
+## 套入程式碼的介紹
+    1. 基本資料類型
+    2. 變數
+    3. object
+    4. function 
+    5. json data intrduction
+    6. 寫log
+    7. 比較運算式
+    8. 字串相加
+    9. 陣列函數
+        * 過濾函數
+        * 轉換函數
+        * iteration函數
--- scripts/title.js
+++ scripts/title.js
@@ -1,7 +1,37 @@
 $(document).ready(function () {
+
+    //* 增加表格列
+    function addRowData(items){
+        console.log("data type:" + typeof items);
+        //json data intrduction
+        //寫log
+        //陣列函數
+        //比較運算式
+        //過濾為職稱
+        var titles = items.filter(function(item){
+            return item.kind == "title"; 
+        })
+        //取得每一筆
+        titles.forEach(function(item) {
+            console.log(item);
+        });
+        //轉換成html要呈現的tag
+        var titleTable = $("#titleTable > tbody");
+        
+        var tagTitles = titles.map(function(title){
+            return "<tr><td>" + title.id +  "</td><td>" + title.name +  "</td></tr>";
+
+        })
+
+        tagTitles.forEach(function(item){
+            titleTable.append(item);
+        })
+    }
+
     $.getJSON("data/items.json",
         function (data, textStatus, jqXHR) {
             console.log(data);
+            addRowData(data);
         }
     );
 });
